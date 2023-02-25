ae5da937e4d403818da65b8de09ff02dd9d79360
VladikaAndrii <andrii_vladika@ukr.net>
2023-02-25 19:07:40 +0200

    Script for change photo types, with requirements.txt

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+
+from PIL import Image
+from pillow_heif import register_heif_opener
+
+list_of_files = os.listdir("photo/")
+dir = "photo/"
+git add README.md
+
+for name_of_file in list_of_files:
+    path = dir + name_of_file
+
+    if name_of_file.split(".")[-1] in ["jpg", "jpeg"]:
+        continue
+    if name_of_file.split(".")[-1] == "heif":
+        register_heif_opener()
+        print(name_of_file + " - Перетворення виконано")
+        with Image.open(path) as file:
+            new_file_name = name_of_file.split(".")[0] + ".jpg"
+            end_path = f"photo/{new_file_name}"
+            file.convert("RGB").save(end_path, "jpeg")
+        os.remove(path)
+    else:
+        print(name_of_file + " - Перетворення виконано")
+        with Image.open(path) as file:
+            new_file_name = name_of_file.split(".")[0] + ".jpg"
+            end_path = f"photo/{new_file_name}"
+            file.convert("RGB").save(end_path, "jpeg")
+        os.remove(path)
+
